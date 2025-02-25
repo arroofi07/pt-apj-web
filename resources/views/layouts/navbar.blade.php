e13d1ec93f4bf886a2145d2c95f35c1a5f531bf7
arovi <arrooficr7@gmail.com>
2025-02-25 10:53:08 +0700

    initial project

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  @vite('resources/css/app.css')
+</head>
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+    <a class="navbar-brand text-red-500 font-bold" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+
+  </nav>
+  
+</body>
+</html>
